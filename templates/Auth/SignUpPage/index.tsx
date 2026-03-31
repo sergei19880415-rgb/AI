c36701cb93e30274931b54f449ef3b44ca0b88b8
sergei19880415-rgb <sergei19880415@gmail.com>
2026-03-31 16:32:08 +0300

    Update index.tsx

--- templates/Auth/SignUpPage/index.tsx
+++ templates/Auth/SignUpPage/index.tsx
@@ -1,46 +1,14 @@
 "use client";
 
-import React, { useState } from "react";
 import Layout from "@/components/Login/Layout";
-import VerifyCode from "@/components/Login/VerifyCode";
-import Success from "@/components/Login/Success";
 import Start from "./Start";
-import About from "./About";
 
-const SignInPage = () => {
-    const [step, setStep] = useState<"start" | "verify" | "about" | "success">(
-        "start"
-    );
-
-    const handleSignUp = () => {
-        setStep("verify");
-    };
-
-    const handleVerifyCode = () => {
-        setStep("about");
-    };
-    const handleAbout = () => {
-        setStep("success");
-    };
-
+const SignUpPage = () => {
     return (
         <Layout>
-            {step === "start" && <Start onSignUp={handleSignUp} />}
-            {step === "verify" && (
-                <VerifyCode
-                    title="Verify your email address"
-                    onContinue={handleVerifyCode}
-                />
-            )}
-            {step === "about" && <About onContinue={handleAbout} />}
-            {step === "success" && (
-                <Success
-                    title="Registration Complete!"
-                    description="Let’s get started with your first AI chat."
-                />
-            )}
+            <Start />
         </Layout>
     );
 };
 
-export default SignInPage;
+export default SignUpPage;
